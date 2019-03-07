473a555426af8880b470858dacef561ed1264f36
chams <alchemist107@gmail.com>
2019-03-07 16:22:38 +0100

    please please git add .

--- monitored-geth-client/start.sh
+++ monitored-geth-client/start.sh
@@ -8,4 +8,4 @@ geth --datadir=~/.ethereum/devchain init "/root/files/genesis.json"
 sleep 3
 BOOTSTRAP_IP=`getent hosts bootstrap | cut -d" " -f1`
 GETH_OPTS=${@/127.0.0.1/$BOOTSTRAP_IP}
-geth $GETH_OPTS --password "/root/files/genesis.json" --mine
+geth $GETH_OPTS 
