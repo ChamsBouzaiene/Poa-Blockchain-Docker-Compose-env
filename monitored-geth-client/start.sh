76359924b7ab577b1cb086b3df368448ac0d98a6
chams <alchemist107@gmail.com>
2019-03-07 16:18:01 +0100

    please git add .

--- monitored-geth-client/start.sh
+++ monitored-geth-client/start.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 set -e
 cd /root/eth-net-intelligence-api
-perl -pi -e "s/XXX/$(hostname)/g" app.json
+perl -pi -e "s/127.0.0.1/$(hostname)/g" app.json
 /usr/bin/pm2 start ./app.json
 sleep 3
 geth --datadir=~/.ethereum/devchain init "/root/files/genesis.json"
 sleep 3
 BOOTSTRAP_IP=`getent hosts bootstrap | cut -d" " -f1`
-GETH_OPTS=${@/XXX/$BOOTSTRAP_IP}
-geth $GETH_OPTS
+GETH_OPTS=${@/127.0.0.1/$BOOTSTRAP_IP}
+geth $GETH_OPTS --password "/root/files/genesis.json" --mine
