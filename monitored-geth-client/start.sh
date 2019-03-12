1621bc7219554d030d515dfdd34ea460f12b39c5
chams <alchemist107@gmail.com>
2019-03-12 14:52:52 +0100

    fsdfs

--- monitored-geth-client/start.sh
+++ monitored-geth-client/start.sh
@@ -8,4 +8,7 @@ geth --datadir=~/.ethereum/devchain init "/root/files/genesis.json"
 sleep 3
 BOOTSTRAP_IP=`getent hosts bootstrap | cut -d" " -f1`
 GETH_OPTS=${@/test/$BOOTSTRAP_IP}
+echo GETH_OPTS
+echo $GETH_OPTS
+
 geth $GETH_OPTS 
