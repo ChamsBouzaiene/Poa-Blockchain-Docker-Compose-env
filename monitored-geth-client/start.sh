e5ee65146d6a618502b88b6a55a704641b4bcf96
chams <alchemist107@gmail.com>
2019-03-12 14:45:06 +0100

    laalla

--- monitored-geth-client/start.sh
+++ monitored-geth-client/start.sh
@@ -7,5 +7,5 @@ sleep 3
 geth --datadir=~/.ethereum/devchain init "/root/files/genesis.json"
 sleep 3
 BOOTSTRAP_IP=`getent hosts bootstrap | cut -d" " -f1`
-
+GETH_OPTS=${@/test/$BOOTSTRAP_IP}
 geth $GETH_OPTS 
