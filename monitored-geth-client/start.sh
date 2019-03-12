a57db39cfd87c0bc4bb99ec9c21c1c6b86eda65d
chams <alchemist107@gmail.com>
2019-03-12 16:01:23 +0100

    wtf

--- monitored-geth-client/start.sh
+++ monitored-geth-client/start.sh
@@ -2,14 +2,10 @@
 set -e
 cd /root/eth-net-intelligence-api
 perl -pi -e "s/XXX/$(hostname)/g" app.json
-
-sleep 3
 /usr/bin/pm2 start ./app.json
 sleep 3
 geth --datadir=~/.ethereum/devchain init "/root/files/genesis.json"
 sleep 3
 BOOTSTRAP_IP=`getent hosts bootstrap | cut -d" " -f1`
 GETH_OPTS=${@/XXX/$BOOTSTRAP_IP}
-
-
-geth $GETH_OPTS 
+geth $GETH_OPTS
